58504315c9cade0e8477eea8030c8b53b9177680
David Grammatico <david.lugdunum@gmail.com>
2022-09-30 19:04:58 +0200

    add interractive_page

---
+++ lib/interractive_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class InterractivePage extends StatefulWidget {
+  const InterractivePage({Key? key}) : super(key: key);
+
+  @override
+  InterractivePageState createState() => InterractivePageState();
+}
+
+class InterractivePageState extends State<InterractivePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
